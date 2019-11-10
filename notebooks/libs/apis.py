86046565bfc847db9a5441b91be626cb2d6ccba8
Christian Cathro <cscathro@gmail.com>
2019-11-10 11:34:22 -0600

    completed the preprocess function for ico class

--- notebooks/libs/apis.py
+++ notebooks/libs/apis.py
@@ -14,6 +14,7 @@ class ICO_data():
     def __init__(self):
         self.url =' https://api.icowatchlist.com/public/v1/' 
         
+        
     def get_json(self):
         '''
         Sends HTTP Request to provided url and returns a json (dictionary) object.
@@ -26,6 +27,7 @@ class ICO_data():
         json_data = json.loads(raw_data)
         return json_data
 
+    
     def get_ico_df(self):
         '''
         Performs the 'get_json()' funtion and converts it into a Pandas DataFrame
@@ -36,35 +38,49 @@ class ICO_data():
         return df
 
 
-#Not functioning properly, need to check the .apply() function and the for loops     
     def preprocess_data(self):
         '''
         Performs the 'get_df' function and removes erronus columns, converts time to DateTime objects and 
-        converts the numbers to floats.
+        converts the numbers to floats
         '''
         df = self.get_ico_df()
-        df.set_index('coin_symbol', inplace = True)
+        
         df.drop(columns=['icowatchlist_url', 'image', 'website_link'], inplace = True)
-        df['start_time'].apply(dt.strptime(df['start_time'], "%Y-%m-%d %H:%M:%S"))
-        df['end_time'].apply(dt.strptime(df['end_time'], "%Y-%m-%d %H:%M:%S"))
-        for i in ico_df['price_usd'].values:
-            if i == 'NA':
-                pass
-            else:
-                try:
-                    price = re.split(',', i)
-                    i = float(price[0] + price[1])
-                except:
-                    i = float(i)
-                
-        for i in df['ROI'].values:
-            try:
-                roi = re.split('[, %]', i)
-                i = float(roi[0] + roi[1])
-            except:
-                pass
-
-            return df
+        reordered_columns = ['Name', 'Description', 'Price(USD)', 'Start', 'End', 'ROI(Pct)', 'Timezone']
+        df.rename(columns={'all_time_roi': 'ROI(Pct)',
+                           'coin_symbol': 'Ticker',
+                          'description': 'Description',
+                           'end_time': 'End',
+                           'name': 'Name',
+                           'price_usd': 'Price(USD)',
+                          'start_time': 'Start',
+                           'timezone': 'Timezone',
+                          }, inplace=True)
+        df.set_index('Ticker', inplace = True)
+        df=df.reindex(columns=reordered_columns)
+        #Convert the strings to datetime objects
+        df['Start'] = df['Start'].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S"))
+        df['End'] = df['End'].apply(lambda x: dt.strptime(x, "%Y-%m-%d %H:%M:%S"))
+        df['Price(USD)'] = df['Price(USD)'].replace("NA",'0')
+        df['ROI(Pct)'] = df['ROI(Pct)'].replace('NA','0%')
+        #Split the price values that are over 1,000 at the ','
+        df['Price(USD)'] = df['Price(USD)'].apply(lambda x: re.split(',', x))
+        #Split the roi values at their ',', and '%'
+        df['ROI(Pct)'] = df['ROI(Pct)'].apply(lambda x: re.split('[, %]', x))
+        #Merge the strings back that are over 1,000 and turn them into floats
+        try:
+            df['Price(USD)'] = df['Price(USD)'].apply(lambda x: float(x[0] + x[1])) 
+        #Convert the remaining strings back to floats    
+        except:
+            df['Price(USD)'] = df['Price(USD)'].apply(lambda x: float(x[0]))
+        
+        try:
+            df['ROI(Pct)'] = df['ROI(Pct)'].apply(lambda x: float(x[0] + x[1])/100) 
+        except:
+            df['ROI(Pct)'] = df['ROI(Pct)'].apply(lambda x: float(x[0])/100)
+        #Create a duration column from the start and end dates
+        df["Duration"] = df['End'] - df['Start']
+        return df
         
         
         
