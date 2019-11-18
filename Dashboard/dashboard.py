b20b2aff07fc328f0ddc6006aaae9fcf1ceded67
marclweimer <marclweimer@users.noreply.github.com>
2019-11-17 18:44:27 -0600

    finished first draft of dash

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -7,69 +7,18 @@ import pandas as pd
 from pathlib import Path
 
 
-path = Path(r'C:\Users\marcl\Documents\fintech\ICO_Viability_Index\data\cleandata\track_cmc_merged_df.csv')
+path = Path(r'C:\Users\marcl\Documents\fintech\ICO_Viability_Index\data\cleandata\success_NN_1H_df.csv')
 df = pd.read_csv(path)
 
 external_stylesheets = [r'C:\Users\marcl\Documents\fintech\ICO_Viability_Index\Dashboard\assets\style.css']
 
-app = dash.Dash(__name__, 
-    external_stylesheets=external_stylesheets
-    )
+app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H1(children='Digital Asset Viability Index Dashboard'),
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
-        dcc.Tab(label='ICO Rating List', value='tab-1-example'),
-        dcc.Tab(label='Trends Analysis', value='tab-2-example'),
-        dcc.Tab(label='Methodology', value='tab-3-example')
-    ]),
-    html.Div(id='tabs-content-example')
-])
-
-def render_content(tab):
-    if tab == 'tab-1-example':
-        return html.Div([
-            html.H3('ICO Rank'),
-            generate_table(df, 100),
-            html.Div(id='datatable-interactivity-container')
-        ])
-    elif tab == 'tab-2-example':
-        return html.Div([
-            html.H3('Trends Analysis'),
-            dcc.Graph(
-                id='graph-2-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
-        ])
-    elif tab == 'tab-3-example':
-        return html.Div([
-            html.H3('Methodology'),
-            dcc.Graph(
-                id='graph-2-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
-        ])
-
-@app.callback(Output('tabs-content-example', 'children'),
-              [Input('tabs-example', 'value')])
-
-#Tab 1 Table Layout Function
-def generate_table(dataframe, max_rows):
-    return dt.DataTable(
+app.layout = html.Div([
+    html.Div(html.Img(src=app.get_asset_url('logo.png'), height=100)),
+    #html.H1(children='Digital Asset Viability Index Dashboard'),
+    dt.DataTable(
         id='datatable-interactivity',
-        sort_action='native',
         editable=True,
         filter_action='native',
         sort_action="native",
@@ -83,25 +32,33 @@ def generate_table(dataframe, max_rows):
         page_current= 0,
         page_size= 10,
         style_cell={
-            'padding': '12px 15px',
+            'padding': '10px 12px',
             'width': 'auto',
             'textAlign': 'center',
             'font-size': '1em',
-            'line-height': '1.2',
-            'font-weight': '400',
+            'line-height': '1',
+            'font-weight': '300',
             'font-family': '"Open Sans", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif',
             'color': 'rgb(50, 50, 50)'
         },
-        columns=[{"name": i, "id": i} for i in dataframe.columns],
-        data=dataframe.to_dict("rows"),
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict("rows"),
+        hidden_columns=['pct1h', 'pct24h', 'pct7d', 
+            'pre_ico_end', 'pre_ico_start', 'Name_y', 
+            'type', 'End', 'Start', 'Duration', 'pre_Duration',
+            'compound', 'negative', 'neutral', 'positive', 'no_of_posts',
+            'longevity', 'custom_index', 'custom_index_scaled', 'success_index',
+            'success_PCA', 'predicted_NN']
         
-    )
+    ),
+    html.H2(children='ICO Insights'),
+    html.Div(id='datatable-interactivity-container')
+])
 
 @app.callback(
     Output('datatable-interactivity', 'style_data_conditional'),
     [Input('datatable-interactivity', 'selected_columns')]
 )
-
 def update_styles(selected_columns):
     return [{
         'if': { 'column_id': i },
@@ -112,36 +69,70 @@ def update_styles(selected_columns):
     Output('datatable-interactivity-container', "children"),
     [Input('datatable-interactivity', "derived_virtual_data"),
      Input('datatable-interactivity', "derived_virtual_selected_rows")])
-
 def update_graphs(rows, derived_virtual_selected_rows):
     if derived_virtual_selected_rows is None:
         derived_virtual_selected_rows = []
 
-        dff = df if rows is None else pd.DataFrame(rows)
-        
-        return [
+    dff = df if rows is None else pd.DataFrame(rows)
+
+    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+              for i in range(len(dff))]
+
+    return [
         dcc.Graph(
-            id=column,
+            id="ICO strength by Origin",
             figure={
                 "data": [
                     {
                         "x": dff["country"],
-                        "y": dff[column],
+                        "y": dff["market_cap"],
                         "type": "bar",
                         "marker": {"color": colors},
                     }
                 ],
                 "layout": {
-                    "xaxis": {"automargin": True},
+                    "xaxis": {
+                        "automargin": True,
+                        "title": {"text": "Country of Origin"}
+                    },
                     "yaxis": {
                         "automargin": True,
-                        "title": {"text": column}
+                        "title": {"text": "Market Cap"}
+                    },
+                    "height": 250,
+                    "margin": {"t": 10, "l": 10, "r": 10},
+                },
+            },
+        ),
+
+        dcc.Graph(
+            id="market cap by rating",
+            figure={
+                "data": [
+                    {
+                        "x": dff["rating"],
+                        "y": dff["market_cap"],
+                        "type": "bar",
+                        "marker": {"color": colors},
+                    }
+                ],
+                "layout": {
+                    "xaxis": {
+                        "automargin": True,
+                        "title": {"text": "Rating"}
+                    },
+                    "yaxis": {
+                        "automargin": True,
+                        "title": {"text": "Market Cap"}
                     },
                     "height": 250,
                     "margin": {"t": 10, "l": 10, "r": 10},
                 },
             },
         )
+ 
+    ]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
